e7af3585124bbe43b648f096933e79ee7adf5fb1
Charles <charles442009@gmail.com>
2021-04-11 21:45:24 -0700

    swaggy commit 1

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// absolutely necessary 
+console.log('Charles is the best maker of rocket patrol tutorials :^)');
+
+// game config
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480
+}
+let game = new Phaser.Game(config);
